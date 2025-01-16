45e6f681a50496964911d39e1e503e60b1f710ce
tusharch12 <tusharch6577@gmail.com>
2025-01-16 23:23:54 +0530

    implement Search feature

--- src/components/appBar.jsx
+++ src/components/appBar.jsx
@@ -1,49 +1,62 @@
-import React from 'react'
-import {Text,
-    View,
-    StyleSheet,
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 
-import Icon from 'react-native-vector-icons/FontAwesome';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
-export default function Appbar(){
-    return (
-        <View style={styles.container}>
-         <View style={styles.startPart}>
-             <MaterialIcon style={styles.iconStyle}  name="search" size={24}/>
-             <Text style={styles.hintText} >Search contacts</Text>
-         </View>
-         <View style={styles.endPart}>
-         <MaterialIcon style={styles.iconStyle} name="mic" size={24}/>
-         <MaterialIcon style={styles.iconStyle}  name="more-vert" size={24}/>
-         </View>
+export default function Appbar() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('search');
+        }}>
+        <View style={styles.startPart}>
+          <MaterialIcon style={styles.iconStyle} name="search" size={24} />
+          <Text style={styles.hintText}>Search contacts</Text>
         </View>
-    );
+      </TouchableOpacity>
+      <View style={styles.endPart}>
+        <MaterialIcon style={styles.iconStyle} name="mic" size={24} />
+        <MaterialIcon style={styles.iconStyle} name="more-vert" size={24} />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-     container: {
-    height:50,
-       marginVertical:10,
-        backgroundColor:'#f3e5e2',
-        marginHorizontal:18,
-        borderRadius:25,
-        justifyContent:'space-between',
-        flexDirection:'row',
-        paddingHorizontal:18,
-        alignItems:'center',
-     },
-     startPart:{
-         flexDirection:'row',
-     },
-     hintText:{
-        paddingHorizontal:15,
-        fontSize:18,
-     },
-     endPart:{
-        flexDirection:'row',
-    },
-    iconStyle:{
-        paddingHorizontal:5
-    }
+  container: {
+    height: 50,
+    marginVertical: 10,
+    backgroundColor: '#f3e5e2',
+    marginHorizontal: 18,
+    borderRadius: 25,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingHorizontal: 18,
+    alignItems: 'center',
+  },
+  startPart: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputTextStyle: {
+    color: '#000',
+  },
+  hintText: {
+    paddingHorizontal: 15,
+    fontSize: 18,
+  },
+  endPart: {
+    flexDirection: 'row',
+  },
+  iconStyle: {
+    paddingHorizontal: 5,
+  },
 });
---
+++ src/navigation/screen/searchPage.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Text,
+  FlatList,
+  ScrollView,
+} from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
+//const
+import staticData from '../../assest/constant';
+
+//component
+import SimpleCard from '../../components/simpleCard';
+
+export default function SearchScreen({navigation}) {
+  const {callLogs, contacts} = staticData;
+  const [allContact, updateList] = useState(contacts);
+
+  const handleQuery = query => {
+    console.log('query', query);
+    const newContact = contacts.filter(item =>
+        item.name.toLowerCase().includes(query.toLowerCase())
+      );
+
+    updateList(newContact);
+  };
+
+  return (
+    <View>
+      <View style={styles.appbarCon}>
+        <MaterialIcon style={styles.appbarIcon} name="arrow-back" size={25} onPress={()=>navigation.goBack()}/>
+        <TextInput
+        style={styles.searchText}
+          placeholder="Search Contact"
+          onChangeText={handleQuery}
+        />
+      </View>
+      <Text style={styles.headingText}>All Contact</Text>
+      <ScrollView>
+        {allContact.map(item => (
+          <SimpleCard key={item.id} item={item} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appbarCon: {
+    flexDirection: 'row',
+    height: 55,
+    backgroundColor: '#f3e5e2',
+    alignItems: 'center',
+  },
+  appbarIcon: {
+    paddingHorizontal: 10,
+  },
+  searchText:{
+    width:300,
+  },
+  headingText:{
+    fontSize:14,
+    paddingHorizontal:12,
+    paddingVertical:12,
+
+    fontWeight:'400'
+  }
+});
